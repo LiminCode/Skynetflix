7ca27e970971de9dd0360bc1a94d3705dcb63761
rwstrain <53712728+rwstrain@users.noreply.github.com>
2021-05-06 13:08:05 -0400

    Update SkynetflixDDL.sql

--- SkynetflixDDL.sql
+++ SkynetflixDDL.sql
@@ -1,5 +1,4 @@
-
-DROP TABLE IF EXISTS plan;
+DROP TABLE IF EXISTS plan CASCADE;
 create table plan
 (
     name            varchar(50),
@@ -8,37 +7,37 @@ create table plan
     primary key (name)
 );
 --
-DROP TABLE IF EXISTS director;
+DROP TABLE IF EXISTS director CASCADE;
 create table director
 (
     id            varchar(5),
     first_name    varchar(20),
     last_name     varchar(20) not null,
-    age           varchar(3) check (age > 0),
+    age           numeric(3,0) check (age > 0),
     primary key (id)
 );
 --
-DROP TABLE IF EXISTS studio;
+DROP TABLE IF EXISTS studio CASCADE;
 create table studio
 (
-    name          varchar(20),
+    name          varchar(50),
     date_founded  date,
     budget        numeric(12,2) check (budget > 0),
     primary key (name)
 );
 --
-DROP TABLE IF EXISTS actor;
+DROP TABLE IF EXISTS actor CASCADE;
 create table actor
 (
     id            varchar(10),
     first_name    varchar(30),
     last_name     varchar(30) not null,
     gender        varchar(1),
-    age           varchar(3) check (age > 0),
+    age           numeric(3,0) check (age > 0),
     primary key (id)
 );
 --
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 create table users
 (
     id            varchar(5),
@@ -50,7 +49,7 @@ create table users
     primary key (id)
 );
 --
-DROP  TABLE IF EXISTS movie;
+DROP  TABLE IF EXISTS movie CASCADE;
 create table movie
 (
     id             varchar(10),
@@ -62,64 +61,64 @@ create table movie
     budget         numeric(12,2) check (budget > 0),
     gross_income   numeric(12,2),
     summary        varchar(255),
-    studio         varchar(20),
+    studio         varchar(50),
     director_id    varchar(10),
     primary key (id),
-    foreign key (studio) references studio (name)
-            on delete set null,
+    foreign key (studio) references studio (name) 
+	    on delete set null,
     foreign key (director_id) references director (id)
-            on delete set null,
+            on delete set null
 );
 --
-DROP TABLE IF EXISTS subscription;
+DROP TABLE IF EXISTS subscription CASCADE;
 create table subscription
 (
     user_id         varchar(10),
     plan_name       varchar(50),
     start_date      date not null,
     purchased_date  date,
-    primary key (user_id, plan_name, start_date)
-    foreign key (user_id) references user (id)
+    primary key (user_id, plan_name, start_date),
+    foreign key (user_id) references users (id)
             on delete cascade,
     foreign key (plan_name) references plan (name)
             on delete cascade
 );
 --
-DROP TABLE IF EXISTS history;
+DROP TABLE IF EXISTS history CASCADE;
 create table history
 (
     user_id         varchar(10),
     movie_id        varchar(10),
     watch_date      date not null,
-    is_finished     boolean,
-    primary key (user_id, movie_id),
-    foreign key (user_id) references user (id)
-            on delete cascade,
-    foreign key (movie_id) references movie (id),
-            on delete cascade
-);
---
-DROP TABLE IF EXISTS review;
-create table review
-(
-    user_id         varchar(10),
-    movie_id        varchar(10),
-    review_date     date,
-    rating          numeric(3,1) check (rating >= 0 and rating <= 100) not null,
-    content         varchar(256),
-    primary key (user_id, movie_id),
-    foreign key (user_id) references user (id)
+    is_finished     varchar(1),
+    primary key (user_id, movie_id, watch_date),
+    foreign key (user_id) references users (id)
             on delete cascade,
     foreign key (movie_id) references movie (id)
             on delete cascade
 );
 --
-DROP TABLE IF EXISTS act;
+DROP TABLE IF EXISTS review CASCADE;
+create table review
+(
+    user_id         varchar(10),
+    movie_id        varchar(10),
+    review_date     date,
+    rating          numeric(4,1) check (rating >= 0 and rating <= 100) not null,
+    content         varchar(256),
+    primary key (user_id, movie_id),
+    foreign key (user_id) references users (id)
+            on delete cascade,
+    foreign key (movie_id) references movie (id)
+            on delete cascade
+);
+--
+DROP TABLE IF EXISTS act CASCADE;
 create table act
 (
     movie_id        varchar(10),
     actor_id        varchar(10),
-    if_main         boolean,
+    if_main         varchar(1),
     role            varchar(50) not null,
     primary key (movie_id, actor_id),
     foreign key (movie_id) references movie (id)
