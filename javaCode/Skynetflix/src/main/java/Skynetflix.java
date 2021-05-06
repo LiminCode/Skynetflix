a3b4c972e4a9e2b9e082289e7ad82227498abc6a
Limin Wang <wlmmarine@live.com>
2021-05-06 10:07:59 -0400

    small database backup

--- javaCode/Skynetflix/src/main/java/Skynetflix.java
+++ javaCode/Skynetflix/src/main/java/Skynetflix.java
@@ -3,8 +3,8 @@ import java.sql.Connection;
 public class Skynetflix {
 	 public static void main(String[] args){
 		 System.out.println("+".repeat(10));
-		 
-		 
+		 Utilities utility = new Utilities();  // for print in color
+		 Task task = new Task();
 		 
 		 // start SSH forwarding if not running on Skynet 
 		 SshPortForward forward = new SshPortForward();
@@ -25,18 +25,95 @@ public class Skynetflix {
 		 while (!quit){
 		        System.out.println("\033[32;4m" + "=".repeat(50) + "\033[0m"+
 		        						"\nWhat would you like to do?\n" +
-		  			                   "\t(a) Generate advisor list\n" 
-		  			                   "Enter a single letter corresponding to one of the above here, " +
-		  			                   "or enter 'q' to exit: ");
+		  			                   "\t(0) Add actors to movie\n" +
+		  			                   "\t(1) add movie\n" +
+		  			                   "\t(2) add user\n" +
+		  			                   "\t(3) delist movie\n" +
+		  			                   "\t(4) ending subscriptions\n" +
+		  			                   "\t(5) generate subscription counts\n" +
+		  			                   "\t(6) get active movies\n" +
+		  			                   "\t(7) get actor director pairs\n" +
+		  			                   "\t(8) get busiest users\n" +
+		  			                   "\t(9) get highest grossing studios\n" +
+		  			                   "\t(10) get highest rated actors\n" +
+		  			                   "\t(11) get highest rated directors\n" +
+		  			                   "\t(12) get highest rated movies\n" +
+		  			                   "\t(13) get popular movies\n" +
+		  			                   "\t(14) get user current subscription window\n" +
+		  			                   "\t(15) get user genres\n" +
+		  			                   "\t(16) leave a review\n" +
+		  			                   "\t(17) relist movie\n" +
+		  			                   "\t(18) remove user\n" +
+		  			                   "Enter an integer or enter 'q' to exit: \n");
 
 		        String call = scan.next();
-
-		        switch(call.toLowerCase().charAt(0)){
-		        	case 'a':
-		        		Task.add_user(conn);
+		        // string -> integer
+		        int number= -1;
+		        try {
+		        	number = Integer.parseInt(call);
+		        }catch(NumberFormatException e) {
+		        	// here means not a integer
+		        	quit = true;
+		        	System.out.println("Not a integer, quit...");
+		        	break;
+		        }
+		        switch(number){ // TODO --- replace the methods when finish
+		        	case 1:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 2:
+		        		task.add_movie(conn);
+		        		break;
+		        	case 3:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 4:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 5:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 6:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 7:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 8:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 9:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 10:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 11:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 12:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 13:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 14:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 15:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 116:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 17:
+		        		task.get_active_movies(conn);
+		        		break;
+		        	case 18:
+		        		task.get_active_movies(conn);
 		        		break;
 			        default:
-			          System.out.println("Not a valid action.");
+			          System.out.println(utility.as_bold_color("[!!!]","r")+"Not a valid action: "+number);
 		        }
 		 }
 		 System.out.println("\n-- -- -- -- -- -- -- --\nExiting...");
--- javaCode/Skynetflix/src/main/java/Task.java
+++ javaCode/Skynetflix/src/main/java/Task.java
@@ -2,23 +2,29 @@ import java.sql.*;
 
 
 public class Task {
+	  Utilities utility = new Utilities();  // for print in color
+	
 	  // Generate a list of all students and their advisors.
-	  public static void add_user(Connection conn){
-		  
+	  public void get_active_movies(Connection conn){
+		  System.out.println(utility.as_bold_color("[iii]","g")+" Query Active Movies ==>");
 		  PreparedStatement stmt=null;
 		  ResultSet res=null;
-		  String sql = "";
+		  String sql = "SELECT title from movie where available=true order by title;";
 		try {
 			stmt = conn.prepareStatement(sql);
 			res = stmt.executeQuery();
+			int i = 1;
+			
 		    while (res.next()){
-			      System.out.println(res.getString(1) + " " + res.getString(2) + " " + res.getString(3));
+			      System.out.println(i+": "+res.getString(1));
+			      // todo: limit the print rows number
+			      i++;
 			}
 		}catch(SQLException e){
-			System.out.println("Query advisor list Failed,SQL error: ");
+			System.out.println("Query Active Movie failed,SQL error: ");
 			e.printStackTrace();
  		}catch(Exception e){
-			System.out.println("Query advisor list Failed, class error: ");
+			System.out.println("Query Active Movie failed, class error: ");
 			e.printStackTrace();
 		}finally{
 			 try{
@@ -27,11 +33,48 @@ public class Task {
 		         if(res!=null)
 			        res.close();
 		      }catch(SQLException e){
-		    	 System.out.println("Query advisor list Failed,SQL error: ");
+		    	 System.out.println("Query Active Movie failed,SQL error: ");
 		    	 e.printStackTrace();
 		      }//end finally try
 		}
-		System.out.println("Query advisor list finished.");
+		System.out.println(utility.as_bold_color("[iii]","g")+" Query Active Movie finished.");
+	  }
+	  
+	  //add_movie
+	  // Generate a list of all students and their advisors.
+	  public void add_movie(Connection conn){
+		  System.out.println(utility.as_bold_color("[iii]","g")+" Add New Movie ==>");
+		  PreparedStatement stmt=null;
+		  ResultSet res=null;
+		  String sql = "SELECT title from movie where available=true order by title;";
+		try {
+			stmt = conn.prepareStatement(sql);
+			res = stmt.executeQuery();
+			int i = 1;
+			
+		    while (res.next()){
+			      System.out.println(i+": "+res.getString(1));
+			      // todo: limit the print rows number
+			      i++;
+			}
+		}catch(SQLException e){
+			System.out.println("Query Active Movie failed,SQL error: ");
+			e.printStackTrace();
+ 		}catch(Exception e){
+			System.out.println("Query Active Movie failed, class error: ");
+			e.printStackTrace();
+		}finally{
+			 try{
+		         if(stmt!=null)
+		            stmt.close();
+		         if(res!=null)
+			        res.close();
+		      }catch(SQLException e){
+		    	 System.out.println("Query Active Movie failed,SQL error: ");
+		    	 e.printStackTrace();
+		      }//end finally try
+		}
+		System.out.println(utility.as_bold_color("[iii]","g")+" Query Active Movie finished.");
 	  }
  
 }
